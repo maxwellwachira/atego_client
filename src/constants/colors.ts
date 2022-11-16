57e52286bf5c85d0291391ea5c79b7c4e7371c64
maxwellwachira <maxwellwachira67@gmail.com>
2022-11-17 02:45:50 +0300

    color and footer constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+    primaryColor: '#31B44B',
+    primaryLight: "#EAFDEE",
+    secondaryColor: '#4385F5',
+};
---
+++ src/constants/footer.ts
@@ -0,0 +1,38 @@
+export const footerData = [
+    {
+        title: 'Site Map', 
+        links: [
+            { id: 0, link: '/', label: 'Home' },
+            { id: 1, link: '/about', label: 'About' },
+            { id: 2, link: '/find-a-home', label: 'Home Finder' },
+            { id: 3, link: '/faq', label: 'FAQ' }
+        ]
+    },
+    {
+        title: 'Courses', 
+        links: [
+            { id: 0, link: '/contact', label: 'all courses' },
+            { id: 1, link: 'mailto:', label: 'PCB Design' },
+            { id: 2, link: 'mailto:', label: 'Firmware' },
+            { id: 2, link: 'mailto:', label: 'IoT' },
+        ]
+    }, 
+    {
+        title: 'Contact', 
+        links: [
+            { id: 0, link: '/contact', label: 'Contact Page' },
+            { id: 1, link: 'mailto:', label: 'Support' },
+            { id: 2, link: 'mailto:', label: 'Info' },
+            { id: 2, link: 'mailto:', label: 'Sales' },
+        ]
+    }, 
+    {
+        title: 'Social Links',
+        links: [
+            { id: 0, link: '', label: 'Twitter' },
+            { id: 1, link: '', label: 'LinkedIn' },
+            { id: 2, link: '', label: 'Instagram' },
+            { id: 3, link: '', label: 'Facebook' }
+        ]
+    },
+];
