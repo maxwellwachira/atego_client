9ebd77e250e4f777102c2611989c96750e3863be
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-17 14:31:59 +0300

    added image domain

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['mygrannyslove.com', 'localhost', 'luddoc-institute.com', 'backend.luddoc-institute.com'],
+  }
 }
 
 module.exports = nextConfig
