c583744aa1187a2a64a06ff04219e331bc94e358
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-17 14:37:39 +0300

    added auth context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,39 @@
 import { useEffect, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { ColorScheme, MantineProvider } from '@mantine/core';
+
 import '../styles/globals.css';
 import PageLoader from '../components/pageLoader/pageLoader';
+import { AuthContextProvider } from '../features/authentication/context/authContextProvider';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
-  const colorScheme :ColorScheme = "light";
+  const colorScheme: ColorScheme = "light";
 
-    useEffect(() => {
-      setTimeout(() => setLoading(true), 700);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => setLoading(true), 700);
+  }, []);
 
   return (
-      <MantineProvider 
-        theme={{ 
+    <AuthContextProvider>
+      <MantineProvider
+        theme={{
           colorScheme,
           fontFamily: 'Montserrat, sans-serif'
-        }} 
-        withGlobalStyles 
+        }}
+        withGlobalStyles
         withNormalizeCSS
       >
-        
-          { !loading ?
-            (<PageLoader />) :
-            (<Component {...pageProps} />)
-          }
+
+        {!loading ?
+          (<PageLoader />) :
+          (<Component {...pageProps} />)
+        }
       </MantineProvider>
+    </AuthContextProvider>
   );
-  
+
 }
 
 export default MyApp;
