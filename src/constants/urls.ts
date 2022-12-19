1d5d47d9499bffed8ac778959f857c3e155f917e
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-19 23:10:35 +0300

    updated urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,8 +3,10 @@ import { getCookie } from "cookies-next";
 const env = process.env.NEXT_PUBLIC_ENV as string;
 
 export const urls = {
-    baseUrl:'http://localhost:5000',
-    frontEnd: 'http://localhost:3000',
+    // baseUrl:'http://localhost:5000',
+    // frontEnd: 'http://localhost:3000',
+    baseUrl:'https://api.ategoschool.com',
+    frontEnd: 'https://ategoschool.com',
     tutorLms: 'https://mygrannyslove.com/wp-json/tutor/v1',
     zoomBaseUrl: 'https://api.zoom.us/v2'
 }
