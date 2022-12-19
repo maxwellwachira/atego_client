c1ac652c16d7a265da9a66d2a21219d9e2aae982
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-19 22:04:44 +0300

    tutors pages

---
+++ src/pages/tutors/live-session.tsx
@@ -0,0 +1,86 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Button, Center, Container, Grid, Paper, Stack, Text } from '@mantine/core';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useViewportSize } from '@mantine/hooks';
+
+import { TutorLayout } from '../../layouts/tutorLayout/tutorLayout';
+import { colors } from '../../constants/colors';
+import { useAuthContext } from '../../features/authentication';
+import livesession from '../../assets/livesession.jpg';
+
+const LiveSession: NextPage = () => {
+    const { auth, userMe } = useAuthContext();
+    const router = useRouter();
+    const { width } = useViewportSize();
+
+    const getGreetings = () => {
+        const date = new Date();
+        const hourString = date.getHours();
+        let greetings = '';
+
+        if (hourString <  12) greetings = "Good Morning";
+        if (hourString >=  12 && hourString < 17) greetings = "Good Afternoon";
+        if (hourString >=  17 && hourString <= 24) greetings = "Good Evening";
+
+        return greetings;
+    }
+
+    useEffect(() =>{
+        // if(!auth || userMe.role !== "tutor") router.push('/auth/logout');
+    }, []);
+
+    // if (!auth || userMe.role !== "tutor") return <></>
+
+    return (
+        <>
+         <Head>
+            <title>Atego | Live session</title>
+            <meta name="description" content="Live session" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <TutorLayout>
+            <Container>
+                <Center>
+                    <Text mt="xl" weight={600} size={25} color={`${colors.primaryColor}`}>{`${getGreetings()} ${userMe.firstName} ${userMe.lastName}`}</Text>
+                </Center>
+                <Paper withBorder p="xl" radius={40} mt={25}>
+                    <Grid gutter={40}>
+                        <Grid.Col md={7}>
+                            <Center>
+                                <Image 
+                                    src={livesession}
+                                    height={width > 768 ? 280 : 250}
+                                    width={width > 768 ? 400 : 300}
+                                    alt="live session"
+                                />
+                            </Center>
+                        </Grid.Col>
+                        <Grid.Col md={5}>
+                            <Stack justify="center" p="xl" style={{minHeight: width > 768 ? 280 : 0}}>    
+                                <Text mt={40}>Click the button to join a live session</Text>
+                                <Button
+                                    component='a'
+                                    href='/meeting/live-session'
+                                    variant='outline'
+                                    color='green'
+                                    mt="xl"
+                                    radius="xl"
+                                    fullWidth
+
+                                >
+                                    Join Live Session
+                                </Button>
+                            </Stack>
+                        </Grid.Col>
+                    </Grid>
+                </Paper>
+            </Container>
+        </TutorLayout>
+        </>
+    )
+}
+
+export default LiveSession;
---
+++ src/pages/tutors/uploads.tsx
@@ -0,0 +1,281 @@
+import { useEffect, useState } from 'react';
+import { Alert, Badge, Button, Center, Container, Divider, FileInput, Grid, Group, Modal, Pagination, Paper, Select, Stack, Text } from '@mantine/core';
+import type { NextPage } from 'next';
+import { useForm } from '@mantine/form';
+import Head from 'next/head';
+import Image from 'next/image';
+import axios from 'axios';
+import { IconAlertCircle, IconCheck, IconUpload } from '@tabler/icons';
+import { useRouter } from 'next/router';
+
+import { TutorLayout } from '../../layouts/tutorLayout/tutorLayout';
+import { colors } from '../../constants/colors';
+import { useAuthContext } from '../../features/authentication';
+import { urls } from '../../constants/urls';
+import UploadsTable from '../../features/uploads/components/uploadsTable/uploadsTable';
+import { useRefreshContext } from '../../features/lms/contexts/refreshDataContexProvider';
+
+interface CategoryData {
+    totalCategories: number;
+    totalPages: number;
+    currentPage: number;
+    categories: {
+        id: string;
+        categoryName: string;
+        createdAt: string;
+        updatedAt: string;
+    }[]
+};
+
+interface UploadData {
+    totalUploads: number;
+    totalPages: number;
+    currentPage: number;
+    uploads: {
+        id: string;
+        fileName: string;
+        fileExtension: string;
+        CategoryId: string;
+        fileType: string;
+        fileSize: number;
+        filePath: string;
+        UserId: string;
+        createdAt: string;
+        updatedAt: string;
+    }[]
+}
+
+interface TableData {
+    id: string;
+    count: number;
+    fileName: string;
+    fileExtension: string;
+    type: string;
+    size: number;
+    createdAt: string;
+}
+
+interface SelectData {
+    value: string;
+    label: string;
+};
+
+const Uploads: NextPage = () => {
+    const [activePage, setPage] = useState(1);
+    const [file, setFile] = useState<File | null>(null);
+    const [categoryData, setCategoryData] = useState<CategoryData | null>(null);
+    const [uploadData, setUploadData] = useState<UploadData | null>(null);
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [response, setResponse] = useState("");
+    const { refreshData, toggleRefreshData } = useRefreshContext();
+    const router = useRouter();
+    const limit = 12;
+
+    const { auth, userMe } = useAuthContext();
+
+    const form = useForm({
+        initialValues: {
+            file: null,
+            CategoryId: '',
+            UserId: ''
+        }
+    });
+
+    const onClose = () => {
+        setOpen(false);
+    }
+
+    const getAllCategories = async() => {
+        try {
+            const { data } = await axios.get(`${urls.baseUrl}/category?page=${1}&limit=${1000}`);
+            setCategoryData(data);
+            // console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getAllUploads = async() => {
+        try {
+            const { data } = await axios.get(`${urls.baseUrl}/upload`);
+            setUploadData(data);
+            // console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const tableData = () => {
+        let data: TableData[] = [];
+        uploadData?.uploads.map(async (el, index) => {
+            let upload = {
+                id: el.id,
+                count: (activePage - 1) * limit + ++index,
+                fileName: el.fileName,
+                fileExtension: el.fileExtension,
+                type: el.fileType,
+                size: Number((el.fileSize / (10**6)).toFixed(2)),
+                createdAt: (new Date(el.createdAt)).toLocaleString()
+            }
+            data.push(upload);
+        });
+        
+        return data;
+    }
+
+    const categorySelectData = () => {
+        let data: SelectData[] = [];
+        categoryData?.categories.map((el, index) => {
+            let selectData = {
+                value: el.id,
+                label: el.categoryName,
+            }
+            data.push(selectData);
+        });
+
+        return data;
+    }
+
+    useEffect(() => {
+        // if(!auth || userMe.role !== "tutor") router.push('/auth/logout');
+        getAllCategories();
+        getAllUploads();
+        form.setFieldValue('UserId', userMe.id);
+    }, [activePage, refreshData]);
+
+
+    const handleSubmit = async() => {
+        if(JSON.stringify(form.errors) === "{}"){
+            setLoading(true);
+            const uploadData  = { ...form.values, file};
+            try {
+                const { data } = await axios.post(`${urls.baseUrl}/upload`, uploadData, {headers: {
+                    'Content-Type': 'multipart/form-data'
+                  }});
+                //console.log(data);
+                toggleRefreshData();
+                setLoading(false);
+                setResponse(data.message);
+                setTimeout(() => {setResponse('')}, 5000);
+            } catch (error) {
+                setLoading(false);
+                console.log(error);
+            }
+        }
+    }
+
+    // if (!auth || userMe.role !== "tutor") return <></>
+
+    return (
+        <>
+         <Head>
+            <title>Atego | Tutor Uploads</title>
+            <meta name="description" content="Atego school tutors uploads" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <TutorLayout>
+            <Container>
+                <Paper withBorder  mt='xl' radius={40}>
+                    <Grid>
+                        <Grid.Col md={6}>
+                            <Center>
+                                <Image 
+                                    src='/uploads.svg'
+                                    height={350}
+                                    width={400}
+                                    alt="uploads icon"
+                                />
+                            </Center>
+                        </Grid.Col>
+                        <Grid.Col md={6}>
+                            <Stack p='xl'>
+                            <Text size={28} color={`${colors.primaryColor}`} weight={600} mt="lg" >Uploads</Text>
+                                <Group position='apart'>
+                                    <Text>Total Uploads:</Text>
+                                    <Badge color="dark">{uploadData?.totalUploads} Uploads</Badge>
+                                </Group>
+                                <Button
+                                    mt="xl"
+                                    radius="xl"
+                                    size='md'
+                                    variant='outline'
+                                    color='green'
+                                    leftIcon={<IconUpload />}
+                                    onClick={() => setOpen(true)}
+                                >
+                                    Click to Upload File
+                                </Button>
+                            </Stack>
+                        </Grid.Col>
+                    </Grid>
+                </Paper>
+                <UploadsTable data={tableData()} type="tutor"/>
+                <Center mt="xl"> 
+                    <Pagination total={uploadData ? uploadData.totalPages : 2} color='gray' page={activePage} onChange={setPage}/>
+                </Center>
+            </Container>
+        </TutorLayout>
+        <Modal
+            opened={open}
+            onClose={onClose}
+            title={<Text size={25} color={`${colors.primaryColor}`} weight={600} ><IconUpload /> Upload File</Text>}
+            size="600px"
+        >
+            <Divider />
+            <Container>
+                {response === 'success' ? (   
+                    <Alert icon={<IconCheck size={16} />} title="Success" color="green">
+                        File added Successfully
+                    </Alert>           
+                ): response ? (
+                    <Alert icon={<IconAlertCircle size={16} />} title="Error!" color="red">
+                        Reason: {response} <br />
+                    </Alert>
+                ): ''}
+                <form onSubmit={form.onSubmit(() => handleSubmit())}>
+                    <Stack>
+                        <FileInput 
+                            withAsterisk
+                            label="Upload File" 
+                            placeholder="Click to select file" 
+                            icon={<IconUpload size={14} />}
+                            mt="lg"
+                            radius={15}
+                            value={file}
+                            onChange={setFile}  
+                        />
+                        <Select 
+                            label="Category"
+                            placeholder='Select Course Category'
+                            withAsterisk
+                            searchable
+                            radius={15}
+                            nothingFound="No options"
+                            {...form.getInputProps('CategoryId', { type: 'input' })}
+                            data={categorySelectData()}
+                            mt="md"
+                            error={form.errors.CategoryId}
+                        />
+                        <Button 
+                            rightIcon={<IconCheck />}
+                            color="green"
+                            my="xl"
+                            type='submit'
+                            loading={loading}
+                            loaderPosition="left"
+                            px="xl"
+                            radius={15}
+                        >
+                            Upload 
+                        </Button>
+                    </Stack>
+                </form>
+            </Container>
+        </Modal>
+        </>
+    )
+
+}
+
+export default Uploads;
